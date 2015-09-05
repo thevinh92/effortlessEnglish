e20ca389a6bcefdbdf7f337564c7e40fcbfebcd2
Vinh Nhon <thevinh92@vinhnhon.local>
2015-09-05 15:34:08 +0700

    Initial Commit

---
+++ effortlessEnglish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  effortlessEnglish
+//
+//  Created by Vinh Nhon on 5/09/15.
+//  Copyright (c) 2015 hdvstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ effortlessEnglish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  effortlessEnglish
+//
+//  Created by Vinh Nhon on 5/09/15.
+//  Copyright (c) 2015 hdvstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
